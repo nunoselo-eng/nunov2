c91228aafc69a82362000c4d412c134c24c6eea1
nunoselo-eng <nunoselo@gmail.com>
2026-09-09 19:25:35 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import Login from './pages/Login';
 import ClientDashboard from './pages/ClientDashboard';
 import LojistaDashboard from './pages/LojistaDashboard';
 import AdminDashboard from './pages/AdminDashboard';
+import RepresentanteDashboard from './pages/RepresentanteDashboard';
 import CreateRequest from './pages/CreateRequest';
+import TrocarSenhaObrigatoria from './pages/TrocarSenhaObrigatoria';
+import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
   // Corrige o "deslogamento" em atalhos salvos na tela inicial (Android):
@@ -38,17 +41,37 @@ export default function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
 
+        {/* Troca de senha obrigatória no primeiro acesso (qualquer papel) */}
+        <Route path="/trocar-senha" element={<TrocarSenhaObrigatoria />} />
+
         {/* Rotas do Cliente */}
-        <Route path="/client-dashboard" element={<ClientDashboard />} />
-        <Route path="/my-requests" element={<ClientDashboard />} />
-        <Route path="/create-request" element={<CreateRequest />} />
+        <Route path="/client-dashboard" element={
+          <ProtectedRoute allowedRole="cliente"><ClientDashboard /></ProtectedRoute>
+        } />
+        <Route path="/my-requests" element={
+          <ProtectedRoute allowedRole="cliente"><ClientDashboard /></ProtectedRoute>
+        } />
+        <Route path="/create-request" element={
+          <ProtectedRoute allowedRole="cliente"><CreateRequest /></ProtectedRoute>
+        } />
 
         {/* Rotas do Lojista */}
-        <Route path="/lojista-dashboard" element={<LojistaDashboard />} />
+        <Route path="/lojista-dashboard" element={
+          <ProtectedRoute allowedRole="lojista"><LojistaDashboard /></ProtectedRoute>
+        } />
 
         {/* Rotas do Administrador */}
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/admin" element={
+          <ProtectedRoute allowedRole="admin"><AdminDashboard /></ProtectedRoute>
+        } />
+        <Route path="/admin-dashboard" element={
+          <ProtectedRoute allowedRole="admin"><AdminDashboard /></ProtectedRoute>
+        } />
+
+        {/* Rota do Representante Comercial */}
+        <Route path="/representante-dashboard" element={
+          <ProtectedRoute allowedRole="representante"><RepresentanteDashboard /></ProtectedRoute>
+        } />
 
         {/* Redirecionamento padrão */}
         <Route path="*" element={<Navigate to="/" replace />} />
